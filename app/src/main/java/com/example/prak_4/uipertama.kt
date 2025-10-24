c8eb01bf1c1e06135fd14b806c69a29ba699a2ca
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-10-24 10:19:44 +0700

    menambahkan teks

--- app/src/main/java/com/example/prak_4/uipertama.kt
+++ app/src/main/java/com/example/prak_4/uipertama.kt
@@ -36,4 +36,9 @@ fun ActivitasPertama(modifier: Modifier){
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(
+            text = stringResource( id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
