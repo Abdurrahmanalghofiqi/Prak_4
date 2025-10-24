313629341bae81478fb3fb2c83e65421aa819312
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-10-24 10:23:16 +0700

    menambahkan Btekss

--- app/src/main/java/com/example/prak_4/uipertama.kt
+++ app/src/main/java/com/example/prak_4/uipertama.kt
@@ -86,4 +86,14 @@ fun ActivitasPertama(modifier: Modifier){
             modifier = Modifier
                 .fillMaxSize()
 
-        )
+        ){
+            Text(
+                text = stringResource( id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
+    }
+
+}
