1bdc34a20de718964f34ff44e286b40581db8483
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-10-24 10:14:14 +0700

    teks

--- app/src/main/java/com/example/prak_4/uipertama.kt
+++ app/src/main/java/com/example/prak_4/uipertama.kt
@@ -36,4 +36,22 @@ fun ActivitasPertama(modifier: Modifier){
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(
+            text = stringResource( id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource( id = R.string.univ),
+            fontSize = 22.sp
+        )
+        Spacer(modifier = Modifier.height(height = 25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth( fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        )
