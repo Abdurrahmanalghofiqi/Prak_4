f742724d6319fb6bac0cce8ca1aff1b4a206fe99
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2025-10-24 10:21:39 +0700

    menambahkan kartu

--- app/src/main/java/com/example/prak_4/uipertama.kt
+++ app/src/main/java/com/example/prak_4/uipertama.kt
@@ -54,4 +54,13 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-        )
+        ) {
+            Row() {
+                val gambar = painterResource( id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
